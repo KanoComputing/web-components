cbc03418d89542c83d9fe5ad3f5b90eaaafce7e2
Paul Varache <paul@kano.me>
2018-05-22 13:41:20 +0100

    Module

---
+++ modulizer.sh
@@ -0,0 +1,8 @@
+modulizer --out . \
+--npm-name @kano/web-components \
+--npm-version 3.0.0-beta.1 \
+--dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.1 \
+--dependency-mapping kwc-icons,@kano/kwc-icons,^3.0.0-beta.1 \
+--dependency-mapping platform.js,platform,^1.3.5 \
+--dependency-mapping js-md5,blueimp-md5,^2.10
+
